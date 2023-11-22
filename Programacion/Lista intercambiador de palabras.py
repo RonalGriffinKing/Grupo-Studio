c9600353442bf5c650fe56ac32f52170ab8c095e
RonalGriffinKing <brionesgriffin459@icloud.com>
2023-11-22 08:47:48 +0100

    update

---
+++ Programacion/Lista intercambiador de palabras.py
@@ -0,0 +1,15 @@
+palabras="Hola(como)estas"
+
+for i in range(len(palabras)):
+    if palabras[i]=="(":
+        parentesis1=i
+    elif palabras[i]==")":
+        parentesis2=i
+
+palabra1=palabras[:parentesis1]
+palabra_central=(palabras[parentesis1+1:parentesis2])[::-1]
+palabra_final=palabras[parentesis2+1:]
+print(palabra1)
+print(palabra_central)
+print(palabra_final)
+print(palabra_final+"("+palabra_central+")"+palabra1)
