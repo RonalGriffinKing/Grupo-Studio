9eb831de2a28dc592eaf838d245c809b3284ba7a
RonalGriffinKing <brionesgriffin459@icloud.com>
2024-02-12 09:55:41 +0100

    update

--- Programacion/Encontra ubicacionen lista.py
+++ Programacion/Encontra ubicacionen lista.py
@@ -1,2 +1,12 @@
-numero=int(input())
-                 
+#Recursividad
+
+import faker as f
+diccionario = {}
+def generador():
+    for i in range(10):
+        diccionario[i] = f.Faker().name()
+
+    diccionario[10] = "Ronal"
+    return diccionario
+
+print(generador())
