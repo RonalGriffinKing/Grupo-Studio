a95ea8e6a5b9e4e5b525fb233f68b8a4ebd1e8bd
RonalGriffinKing <brionesgriffin459@icloud.com>
2024-02-12 13:11:59 +0100

    update

---
+++ Base de datos/Local.session.sql
@@ -0,0 +1,18 @@
+
+SELECT * FROM alumno where apellido1="Martinez";
+
+SELECT * from alumno where id=9;
+
+SELECT nombre,fecha_nacimiento FROM alumno WHERE fecha_nacimiento > '1997/01/01'; 
+
+#TODOS LOS ALUMNOS CON ID 1
+
+SELECT * FROM alumno WHERE id=1;
+
+#Devuelve todos los alumnos cuyo telefono 
+
+SELECT
+
+
+
+
---
+++ Base de datos/clase 12-02.sql
@@ -0,0 +1,5 @@
+
+SELECT * FROM alumno where apellido1="Martinez";
+
+SELECT * from alumno where id=9;
+
