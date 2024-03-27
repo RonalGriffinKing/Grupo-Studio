45c1cc1dde38bbf935d8a40b27860f3d4b13d955
RonalGriffinKing <brionesgriffin459@icloud.com>
2024-03-27 13:32:52 +0100

    update

--- JavaScript/Formularios/funciones.js
+++ JavaScript/Formularios/funciones.js
@@ -4,6 +4,7 @@ let selecion = document.getElementById("selecion");
 selecion.addEventListener("change",()=>{
     if(selecion.value=="uno"){
         document.getElementById("contenedor-buscotrabajo").style.display="block"
+        
         document.getElementById("contenedor-buscopersonal").style.display="none"
         document.getElementById("contenedor-suscribirme").style.display="none"
     }
